e631847c847273ae803d0dcb11d7b02c0f0eeea1
knechtel <maiquelknechtel@gmail.com>
2023-01-11 15:18:42 -0300

    Adicionando lógica de login.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
 import './App.css'
+import { CLIENT_ALL } from './util/url'; 
 //const logo: string = require("./logo.svg").default;
-
+import axios from 'axios';
 class App extends Component {
+
+  async componentDidMount() : Promise<void> {
+     await axios.get(CLIENT_ALL, {
+        auth: {
+          username: "maiquel",
+          password: "123",
+        }}).then(res => {
+        const data = res.data;
+        console.log(data)
+      })
+  }
   render() {
     return (
       <div className="App">
         <table>
         <tr>
-        <td>Login</td>
-        <td><input type="text"/></td>
+          <td>Login</td>
+          <td><input type="text"/></td>
         </tr>
         <tr>
-        <td>senha</td>
-        <td><input type="text"/></td>
+          <td>senha</td>
+          <td><input type="text"/></td>
         </tr>
         <input type="submit"/>
         </table>
