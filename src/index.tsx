ff14cb70767202cb592f20c66da76489174724e8
knechtel <maiquelknechtel@gmail.com>
2023-01-14 19:14:44 -0300

    Adicionando rota e lógica de login.

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,14 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import FunctionalComponent from "./component/FunctionalComponent";
+import ErrorComponent from "./component/ErrorComponent";
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}></Route>
-      <Route path="/test" element={<FunctionalComponent message="john" />}></Route>
+      <Route path="/main" element={<FunctionalComponent message="john" />}></Route>
+      <Route path="/error" element={<ErrorComponent />}></Route>
     </Routes>
   </BrowserRouter >
 );
