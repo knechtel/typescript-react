a67bcc633f548d5e04cc9f370072212e05ce58cf
knechtel <maiquelknechtel@gmail.com>
2023-01-14 18:36:42 -0300

    Adicionando componente com mensagem de erro.

---
+++ src/component/ErrorComponent.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+
+
+
+const ErrorComponent: FC = () => {
+    return (
+        <>
+            <h1>Senha e usuário incorretos </h1>
+        </>
+    );
+};
+
+export default ErrorComponent;
