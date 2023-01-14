b7933d0c88216356ee763ec99b9bad00d9feb593
knechtel <maiquelknechtel@gmail.com>
2023-01-14 17:32:22 -0300

    Adicionando rotas.

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import FunctionalComponent from "./component/FunctionalComponent";
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}></Route>
+      <Route path="/test" element={<FunctionalComponent message="john" />}></Route>
+    </Routes>
+  </BrowserRouter >
 );
 reportWebVitals()
