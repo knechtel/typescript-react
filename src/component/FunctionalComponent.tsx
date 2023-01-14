4fdb509d4318df21ef13a41a6fc41e851deac715
knechtel <maiquelknechtel@gmail.com>
2023-01-14 19:12:56 -0300

    Testando lógica dataTable.

--- src/component/FunctionalComponent.tsx
+++ src/component/FunctionalComponent.tsx
@@ -1,13 +1,56 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
+import DataTable from 'react-data-table-component';
+import axios from 'axios';
+import { CLIENT_ALL } from '../util/url';
 
+const columns = [
+    {
+        name: 'Title',
+        selector: (row: { title: any; }) => row.title,
+    },
+    {
+        name: 'Year',
+        selector: (row: { year: any; }) => row.year,
+    },
+];
+
+const data = [
+    {
+        id: 1,
+        title: 'Beetlejuice',
+        year: '1988',
+    },
+    {
+        id: 2,
+        title: 'Ghostbusters',
+        year: '1984',
+    },
+]
 interface MessageProps {
     message: string;
 }
 
 const FunctionalComponent: FC<MessageProps> = ({ message }) => {
+
+    useEffect(() => {
+        axios.get(CLIENT_ALL, { auth: { username: 'maiquel', password: '123' } })
+            .then(function (response) {
+                // handle success
+                console.log(response);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+
+    });
+
     return (
         <>
-            <h1>Welcome {message}</h1>
+            <DataTable
+                columns={columns}
+                data={data}
+            />
         </>
     );
 };
